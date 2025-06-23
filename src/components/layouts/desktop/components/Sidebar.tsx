4bebdc1c3b12c41493cf1449f62bc13d6f7341ec
thi9053 <levanthi.tl1211@proton.me>
2025-06-23 22:26:46 +0700

    FE-11: Create desktop layout

---
+++ src/components/layouts/desktop/components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import { Layout } from "antd";
+import { SidebarMenu } from "./SidebarMenu";
+import { AlignRight, AtSign } from "lucide-react";
+
+const { Sider } = Layout;
+
+export const Sidebar = () => {
+  return (
+    <Sider width={76}>
+      <div className="h-full min-h-[480px] fixed top-0 left-0 flex flex-col items-center !bg-[var(--background-secondary)]">
+        <div className="flex-none py-4 w-[var(--sidebar-width)] flex justify-center">
+          <AtSign size={34} className="text-icon-primary" />
+        </div>
+        <SidebarMenu />
+        <div className="flex-none mb-[22px]">
+          <div className="w-[54px] h-[54px] flex justify-center items-center cursor-pointer group">
+            <AlignRight
+              size={24}
+              className="group-hover:text-icon-primary text-icon-secondary duration-100"
+            />
+          </div>
+        </div>
+      </div>
+    </Sider>
+  );
+};
